a3ec78312ab47dc94907de2939c539a5c78b72f0
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-17 08:26:08 +0100

    testing React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Tasks from './components/Tasks'
+import './App.css'
 
 const App = () => {
-  const message = "Hello world"
-  return <h1>{message}</h1>
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Study',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Read',
+      completed: true,
+    }
+  ]) 
+  
+  return (
+    <div>
+        <div className='container'>
+          <Tasks tasks={tasks}/>
+        </div>
+        
+    </div>
+  )
 } 
 
 export default App;
