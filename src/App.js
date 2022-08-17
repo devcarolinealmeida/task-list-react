2f7bcfe8b533254bb3fbac730a62faabdc376250
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-17 08:38:58 +0100

    add input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Tasks from './components/Tasks'
 import './App.css'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -19,6 +20,7 @@ const App = () => {
   return (
     <div>
         <div className='container'>
+          <AddTask />
           <Tasks tasks={tasks}/>
         </div>
         
