18efc2748c39242054f6572a3068bfc66ec830d5
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-18 15:07:02 +0100

    add button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+import Button from "./Button";
+
 import './AddTask.css'
 
 const AddTask = () => {
     return (
         <div className="add-task-container">
           <input className="add-task-input" type="text" />
+          <div className="add-task-button-container">
+            <Button>Adicionar</Button>
+          </div>
         </div>
       );
 }
