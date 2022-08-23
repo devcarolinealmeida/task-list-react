21a9b66d87bfcb41e320e470d23069af3cde5534
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-23 15:38:20 +0200

    add useEffect to conect API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 
 import Header from './components/Header'
@@ -20,6 +21,13 @@ const App = () => {
     }
   ]) 
 
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const {data} = await axios.get("https://jsonplaceholder.cypress.io/todos?_limit=10")
+      setTasks(data)
+    }
+    fetchTasks()
+  }, [])
  
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
